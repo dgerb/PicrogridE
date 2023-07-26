c3c794663d0a551f5bc6b1444acf56d60542f473
bng18gh <116107377+bng18gh@users.noreply.github.com>
2023-07-26 13:19:36 -0700

    added comments, removed unnecessary libraries

--- Communication Protocol/sample_server.py
+++ Communication Protocol/sample_server.py
@@ -1,15 +1,16 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-import requests
 import json
 
 #change based on which connection you want
-#keep in mind if wifi off, ethernet IP Address changes
-ethhost = "169.254.211.11"
-#wifihost = "198.128.205.144"
-
-
+#keep in mind if wifi off, use ethernet IP address
+#example IP addresses here, use the one matched to your device
+eth_host = "169.254.211.11"
+#wifi_host = "198.128.205.144"
 port = 8000
-class testHTTP(BaseHTTPRequestHandler):
+
+
+class serverHTTP(BaseHTTPRequestHandler):
+    #GET command sends charge command to RPi in json format
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "application/json")
@@ -18,6 +19,7 @@ class testHTTP(BaseHTTPRequestHandler):
         self.wfile.write(bytes('{"Command": "' + str(command) + '"}', "utf-8"))
         
     def do_POST(self):
+        #POST command reads state and rate of charge from battery in json format
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
@@ -25,12 +27,15 @@ class testHTTP(BaseHTTPRequestHandler):
         response = self.rfile.read(content_length)
         response = response.decode('utf-8')
         response = json.loads(response)
+        #prints are just checks to ensure communication, actual server will do manipulation
         print("Battery State of Charge:", response["State"])
         print("Battery Charging Rate:", response["Rate"])
        
         
-server = HTTPServer((ethhost, port), testHTTP)
-#server = HTTPServer((wifihost, port), testHTTP)
+server = HTTPServer((eth_host, port), serverHTTP)
+#server = HTTPServer((wifi_host, port), serverHTTP)
+
+
 print("Server now running")
 server.serve_forever()
 server.server_close()
